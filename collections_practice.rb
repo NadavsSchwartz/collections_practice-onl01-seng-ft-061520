d66d93787d81f0052bac28fa91a22181f325c9a6
Nadav Schwartz <nadavschwartz58@gmail.com>
2020-06-24 16:27:54 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,27 @@
+def sort_array_asc(arr)
+arr.sort
+end
 
+def sort_array_desc(arr)
+  arr.sort {|a, b| b <=> a }
+end 
+
+def sort_array_char_count(arr)
+  arr.sort { |a,b| a.length <=> b.length }
+end
+
+def swap_elements(arr)
+  new_arr = []
+  new_arr.replace([arr[0],arr[2],arr[1]])
+  new_arr
+end
+
+def reverse_array(arr)
+  arr.reverse
+end
+
+def kesha_maker(arr, sep = "$")
+  array.each do |item|
+    item[2] = "$"
+  end
+end
